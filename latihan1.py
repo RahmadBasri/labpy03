d425f19b35bbfb6a139f2faea1feda076a672b85
RahmadBasri <57025005+RahmadBasri@users.noreply.github.com>
2019-11-07 13:49:40 +0700

    latihan1.py
    n=int(input("masukan nilai N:"))
    from random import random
    for i in range(n):
        while 1  :
            n = random()
            if n < 0.5:
                break
        print("data ke :",i,">>",n)
    print("SELESAI")

---
+++ latihan1.py
@@ -0,0 +1,9 @@
+n=int(input("masukan nilai N:"))
+from random import random
+for i in range(n):
+    while 1  :
+        n = random()
+        if n < 0.5:
+            break
+    print("data ke :",i,">>",n)
+print("SELESAI")
